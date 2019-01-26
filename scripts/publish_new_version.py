d2c5d687560295c549ded677d4156b725a0e9aa8
Derrick Chambers <djachambeador@gmail.com>
2019-01-25 17:20:25 -0800

    blacken publish script

--- scripts/publish_new_version.py
+++ scripts/publish_new_version.py
@@ -7,5 +7,5 @@ from subprocess import run
 
 if __name__ == "__main__":
     base = Path(__file__).parent.parent
-    run('python setup.py sdist bdist_wheel', shell=True, cwd=base)
-    run('twine upload dist/*', shell=True, cwd=base)
+    run("python setup.py sdist bdist_wheel", shell=True, cwd=base)
+    run("twine upload dist/*", shell=True, cwd=base)
