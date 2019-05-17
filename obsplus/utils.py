f8c431973aa2e3f6230349215e8d3633e4b2ae90
Derrick Chambers <djachambeador@gmail.com>
2019-05-17 09:38:23 -0700

    bulk waveform refactor

--- obsplus/utils.py
+++ obsplus/utils.py
@@ -5,12 +5,14 @@ import contextlib
 import fnmatch
 import hashlib
 import os
+import re
 import sys
 import textwrap
 import threading
 import warnings
-from functools import singledispatch, wraps, lru_cache
+from functools import singledispatch, wraps, lru_cache, reduce
 from itertools import product
+from operator import add
 from pathlib import Path
 from typing import (
     Union,
@@ -24,13 +26,15 @@ from typing import (
     Set,
     TypeVar,
     Collection,
+    List,
+    Iterable,
 )
 
 import numpy as np
 import obspy
 import obspy.core.event as ev
 import pandas as pd
-from obspy import UTCDateTime as UTC
+from obspy import UTCDateTime as UTC, Stream
 from obspy.core.inventory import Station, Channel
 from obspy.geodetics import gps2dist_azimuth
 from obspy.io.mseed.core import _read_mseed as mread
@@ -47,6 +51,7 @@ from obsplus.constants import (
     inventory_type,
     DISTANCE_COLUMNS,
     DISTANCE_DTYPES,
+    waveform_request_type,
 )
 
 BASIC_NON_SEQUENCE_TYPE = (int, float, str, bool, type(None))
@@ -625,6 +630,65 @@ def _replace_inv_nulls(inv, null_codes=NULL_NSLC_CODES, replacement_value=""):
     return inv
 
 
+def filter_index_bulk(
+    self, bulk: List[str], index: Optional[pd.DataFrame] = None, **kwargs
+) -> Stream:
+    """
+    Get a large number of waveforms with a bulk request.
+
+    Parameters
+    ----------
+    bulk
+        A list of any number of lists containing the following:
+        (network, station, location, channel, starttime, endtime).
+    index
+        A dataframe returned by read_index. Enables calling code to only
+        read the index from disk once for repetitive calls.
+    """
+    if not bulk:  # return emtpy waveforms if empty list or None
+        return obspy.Stream()
+
+    def _func(time, ind, df):
+        """ return waveforms from df of bulk parameters """
+        breakpoint()
+        match_chars = {"*", "?", "[", "]"}
+        ar = np.ones(len(ind))  # indices of ind to use to load data
+        t1, t2 = time[0], time[1]
+        df = df[(df.t1 == time[0]) & (df.t2 == time[1])]
+        # determine which columns use any matching or other select features
+        uses_matches = [_column_contains(df[x], match_chars) for x in NSLC]
+        match_ar = np.array(uses_matches).any(axis=0)
+        df_match = df[match_ar]
+        df_no_match = df[~match_ar]
+        # handle columns that need matches (more expensive)
+        if not df_match.empty:
+            match_bulk = df_match.to_records(index=False)
+            mar = np.array([filter_index(ind, *tuple(b)[:4]) for b in match_bulk])
+            ar = np.logical_and(ar, mar.any(axis=0))
+        # handle columns that do not need matches
+        if not df_no_match.empty:
+            nslc1 = set(get_nslc_series(df_no_match))
+            nslc2 = get_nslc_series(ind)
+            ar = np.logical_and(ar, nslc2.isin(nslc1))
+        return self._index2stream(ind[ar], t1, t2)
+
+    # get a dataframe of the bulk arguments, convert time to float
+    df = pd.DataFrame(bulk, columns=list(NSLC) + ["utc1", "utc2"])
+    df["t1"] = df["utc1"].apply(float)
+    df["t2"] = df["utc2"].apply(float)
+    # read index that contains any times that might be used, or filter
+    # provided index
+    t1, t2 = df["t1"].min(), df["t2"].max()
+    if index is not None:
+        ind = index[~((index.starttime > t2) | (index.endtime < t1))]
+    else:
+        ind = self.read_index(starttime=t1, endtime=t2)
+    # groupby.apply calls two times for each time set, avoid this.
+    unique_times = np.unique(df[["t1", "t2"]].values, axis=0)
+    streams = [_func(time, df=df, ind=ind) for time in unique_times]
+    return reduce(add, streams)
+
+
 def filter_index(
     index: pd.DataFrame,
     network: Optional = None,
@@ -636,7 +700,7 @@ def filter_index(
     **kwargs,
 ) -> np.array:
     """
-    Filter an Index dataframe based on nslc codes and start/end times.
+    Filter a waveform index dataframe based on nslc codes and start/end times.
 
     Parameters
     ----------
@@ -908,3 +972,9 @@ def md5_directory(
         if keep:
             out[str(sub_path.relative_to(path))] = md5(sub_path)
     return out
+
+
+def _column_contains(ser: pd.Series, str_sequence: Iterable[str]) -> pd.Series:
+    """ Test if a str series contains any values in a sequence """
+    safe_matches = {re.escape(x) for x in str_sequence}
+    return ser.str.contains("|".join(safe_matches)).values
--- obsplus/waveforms/get_waveforms.py
+++ obsplus/waveforms/get_waveforms.py
@@ -1,12 +1,18 @@
 """
 Module for adding the client-like "get_waveforms" to the Stream class
 """
-from typing import Optional
+from typing import Optional, List
+from functools import reduce
+from operator import add
 
 import obspy
+import pandas as pd
+import numpy as np
 from obspy import Stream, UTCDateTime as UTC
+from obsplus.utils import _column_contains, get_nslc_series, filter_index
+from obsplus.waveforms.utils import _stream_data_to_df
 
-from obsplus.constants import BIG_UTC, SMALL_UTC
+from obsplus.constants import BIG_UTC, SMALL_UTC, NSLC
 
 
 def get_waveforms(
@@ -51,15 +57,62 @@ def get_waveforms(
     return st
 
 
-def get_waveforms_bulk(stream, bulk_args):
-    """ get bulk waveforms from waveforms """
-    st = obspy.Stream()
-    for arg in bulk_args:
-        stt = get_waveforms(stream, *arg)
-        # if a stream was returned then add it to original
-        if stt is not None:
-            st += stt
-    return st
+def get_waveforms_bulk(st: Stream, bulk: List[str], **kwargs) -> Stream:
+    """
+    Get a large number of waveforms with a bulk request.
+
+    Parameters
+    ----------
+    bulk
+        A list of any number of lists containing the following:
+        (network, station, location, channel, starttime, endtime).
+    index
+        A dataframe returned by read_index. Enables calling code to only
+        read the index from disk once for repetitive calls.
+    """
+    if not bulk:  # return emtpy waveforms if empty list or None
+        return obspy.Stream()
+
+    def _func(time, ind, df, st):
+        """ return waveforms from df of bulk parameters """
+        match_chars = {"*", "?", "[", "]"}
+        ar = np.ones(len(ind))  # indices of ind to use to load data
+        t1, t2 = time[0], time[1]
+        df = df[(df.t1 == time[0]) & (df.t2 == time[1])]
+        # determine which columns use any matching or other select features
+        uses_matches = [_column_contains(df[x], match_chars) for x in NSLC]
+        match_ar = np.array(uses_matches).any(axis=0)
+        df_match = df[match_ar]
+        df_no_match = df[~match_ar]
+        # handle columns that need matches (more expensive)
+        if not df_match.empty:
+            match_bulk = df_match.to_records(index=False)
+            mar = np.array([filter_index(ind, *tuple(b)[:4]) for b in match_bulk])
+            ar = np.logical_and(ar, mar.any(axis=0))
+        # handle columns that do not need matches
+        if not df_no_match.empty:
+            nslc1 = set(get_nslc_series(df_no_match))
+            nslc2 = get_nslc_series(ind)
+            ar = np.logical_and(ar, nslc2.isin(nslc1))
+        # get a list of used traces, combine and trim
+        st = obspy.Stream([x for x, y in zip(st, ar) if y])
+        return st.slice(starttime=UTC(t1), endtime=UTC(t2))
+
+    # get a dataframe of stream contents
+    index = _stream_data_to_df(st)
+    # get a dataframe of the bulk arguments, convert time to float
+    df = pd.DataFrame(bulk, columns=list(NSLC) + ["utc1", "utc2"])
+    df["t1"] = df["utc1"].apply(float)
+    df["t2"] = df["utc2"].apply(float)
+    t1, t2 = df["t1"].min(), df["t2"].max()
+    # filter index and streams to be as short as possible
+    needed = ~((index.starttime > t2) | (index.endtime < t1))
+    ind = index[needed]
+    st = obspy.Stream([tr for tr, bo in zip(st, needed.values) if bo])
+    # groupby.apply calls two times for each time set, avoid this.
+    unique_times = np.unique(df[["t1", "t2"]].values, axis=0)
+    streams = [_func(time, df=df, ind=ind, st=st) for time in unique_times]
+    return reduce(add, streams)
 
 
 # --- add get_waveforms to Stream class
