ea972c3eae942a8c014d5864dc3812ebc829556a
Derrick <d-chambers@users.noreply.github.com>
2019-08-02 20:49:53 -0700

    Update interfaces (#118)
    * use custom metaclass for instance check

--- obsplus/interfaces.py
+++ obsplus/interfaces.py
@@ -4,8 +4,11 @@ Some common interfaces for event/client types.
 Note: These are used instead of the ones in obspy.clients.base so the subclass
 hooks can be used.
 """
-from abc import ABC, abstractmethod
+import inspect
+from abc import abstractmethod
+
 import obspy
+import pandas as pd
 
 
 def add_func_name(my_set: set):
@@ -18,50 +21,69 @@ def add_func_name(my_set: set):
     return _wrap
 
 
-class _MethodChecker:
+class _MethodChecker(type):
     """ class for checking if methods exist """
 
-    required_methods: set = set()
+    def __init__(cls, name, bases, arg_dict):
+        # get all methods defined by class
+        methods = {
+            i for i, v in arg_dict.items() if not i.startswith("_") and callable(v)
+        }
+        cls._required_methods = methods
 
-    @classmethod
-    def __subclasshook__(cls, C):
-        methods = {x for B in C.__mro__ for x in B.__dict__}
-        if cls.required_methods.issubset(methods):
+    def __subclasscheck__(cls, subclass):
+        if not inspect.isclass(subclass):
+            return False
+        methods = {x for B in subclass.__mro__ for x in B.__dict__}
+        if cls._required_methods.issubset(methods):
             return True
-        else:
-            return NotImplemented
+        return False
+
+    def __instancecheck__(cls, instance):
+        is_instance = not inspect.isclass(instance)
+        if is_instance and set(cls._required_methods).issubset(dir(instance)):
+            return True
+        return False
 
 
-class EventClient(ABC, _MethodChecker):
+class EventClient(metaclass=_MethodChecker):
     """ The event client interface """
 
-    required_methods = {"get_events"}
+    @abstractmethod
+    def get_events(self, *args, **kwargs) -> obspy.Catalog:
+        """ A method which must return events as obspy.Catalog object. """
 
 
-class WaveformClient(ABC, _MethodChecker):
-    """ The waveform client interface"""
+class WaveformClient(metaclass=_MethodChecker):
+    """ The waveform client interface. """
 
-    required_methods = {"get_waveforms"}
+    @abstractmethod
+    def get_waveforms(
+        self, network, station, location, channel, starttime, endtime
+    ) -> obspy.Stream:
+        """ A method which must return waveforms as an obspy.Stream. """
 
 
-class StationClient(ABC, _MethodChecker):
+class StationClient(metaclass=_MethodChecker):
     """ The station client interface """
 
-    required_methods = {"get_stations"}
+    @abstractmethod
+    def get_stations(self, *args, **kwargs) -> obspy.Inventory:
+        """ A method which must return an inventory object. """
 
 
-class BankType(ABC, _MethodChecker):
+class BankType(metaclass=_MethodChecker):
     """ an object that looks like a bank """
 
-    required_methods = {"read_index"}
+    @abstractmethod
+    def read_index(self, *args, **kwargs) -> pd.DataFrame:
+        """ A method which must return a dataframe of index contents. """
 
 
-class ProgressBar(ABC, _MethodChecker):
+class ProgressBar(metaclass=_MethodChecker):
     """
     A class that behaves like the progressbar2.ProgressBar class.
     """
-
-    required_methods: set = {"update", "finish"}
 
     @abstractmethod
     def update(self, value=None, force=False, **kwargs):
@@ -73,6 +95,6 @@ class ProgressBar(ABC, _MethodChecker):
 
 
 # register virtual subclasses
-WaveformClient.register(obspy.Stream)
-EventClient.register(obspy.Catalog)
-StationClient.register(obspy.Inventory)
+# WaveformClient.register(obspy.Stream)
+# EventClient.register(obspy.Catalog)
+# StationClient.register(obspy.Inventory)
