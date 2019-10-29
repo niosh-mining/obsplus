9cff773d6c459b962a649474be132a40c51fe213
derrick <djachambeador@gmail.com>
2019-10-28 21:35:10 -0700

    update docs, start tests

--- obsplus/structures/waveframe/waveframe.py
+++ obsplus/structures/waveframe/waveframe.py
@@ -5,8 +5,8 @@ import obspy
 import pandas as pd
 
 from obsplus.constants import waveform_clientable_type, BULK_WAVEFORM_COLUMNS
+from obsplus.structures.waveframe.utils import _time_to_utc
 from obsplus.utils import order_columns
-from obsplus.structures.waveframe.utils import _time_cols_to_datetime64, _time_to_utc
 
 
 class WaveFrame:
@@ -18,9 +18,7 @@ class WaveFrame:
         # first get bulk request arguments
         stats = _time_to_utc(order_columns(stats, BULK_WAVEFORM_COLUMNS))
         nslc = stats[list(BULK_WAVEFORM_COLUMNS)].to_records(index=False).tolist()
-        st = waveform_client.get_bulk_waveforms(nslc)
-
-        breakpoint()
+        # st = waveform_client.get_bulk_waveforms(nslc)
 
     # --- Alternative constructors
     @classmethod
