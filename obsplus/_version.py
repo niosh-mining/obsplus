65dba331a9c2e5cdfc482db3fdf6e65aabedb857
Derrick Chambers <djachambeador@gmail.com>
2020-02-16 18:45:51 -0800

    versioneer

--- obsplus/_version.py
+++ obsplus/_version.py
@@ -40,7 +40,7 @@ def get_config():
     cfg = VersioneerConfig()
     cfg.VCS = "git"
     cfg.style = "pep440"
-    cfg.tag_prefix = "None"
+    cfg.tag_prefix = ""
     cfg.parentdir_prefix = "obsplus-"
     cfg.versionfile_source = "obsplus/_version.py"
     cfg.verbose = False
