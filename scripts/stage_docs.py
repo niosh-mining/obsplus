8c73a7205f89b763509041278c22e796c97e5b56
Derrick Chambers <djachambeador@gmail.com>
2020-02-18 15:58:30 -0800

    fix stagedocs

--- scripts/stage_docs.py
+++ scripts/stage_docs.py
@@ -154,7 +154,7 @@ def stage_docs(build_path=None, pages_path=None, remove_dirty: bool = False) -> 
     """
     # get paths
     base = Path(__file__).absolute().parent.parent
-    build_path = Path(build_path) or Path(make_docs()).parent
+    build_path = Path(make_docs()).parent if build_path is None else build_path
     pages_path = Path(pages_path or base.parent / "obsplus_documentation")
     _make_gh_pages_repo(base, pages_path)
     # copy build html directory
