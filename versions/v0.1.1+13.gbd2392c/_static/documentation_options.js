0abf24e9afdcde708ecbaf7d4e66fa4c7ee6f0de
GithubActions <donotemailme@example.com>
2021-03-16 16:18:29 +0000

    v0.2.0+3.g4e9cb02 docs

---
+++ versions/v0.1.1+13.gbd2392c/_static/documentation_options.js
@@ -0,0 +1,12 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: 'v0.1.1+13.gbd2392c.dirty',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false
+};
